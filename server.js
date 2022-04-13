8b0e88aff955647459c849ca114693f99639a4fb
leesebas <leesebas@amazon.com>
2022-04-13 16:29:50 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const http = require('http');
+
+const hostname = '0.0.0.0';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+const signals = ["SIGTERM", "SIGINT", "SIGHUP"];
+
+const shutdown = async (signal) => {
+    console.log(`Signal received: ${signal}`);
+    process.exit(0);
+};
+
+signals.forEach((signal) => {
+    process.on(signal, shutdown);
+});
